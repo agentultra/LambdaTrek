b7bd9312b1329d076d891cb273d2eed8305ab03a
James King <james@agentultra.com>
2023-10-24 21:39:41 -0400

    Update Quadrant buildTiles

--- src/LambdaTrek/Simulation/Quadrant.hs
+++ src/LambdaTrek/Simulation/Quadrant.hs
@@ -7,6 +7,7 @@
 module LambdaTrek.Simulation.Quadrant where
 
 import Data.Array hiding ((!))
+import Data.List ((!!))
 import qualified Data.Array as Array
 import Data.List.Split
 import Data.Text (Text)
@@ -19,6 +20,8 @@ import LambdaTrek.Simulation.Sector
 import LambdaTrek.Simulation.Station
 import Lens.Micro.TH
 
+import qualified Debug.Trace as Debug
+
 data Quadrant
   = Quadrant
   { _quadrantStars :: Map (Int, Int) [(Int, Int)]
@@ -27,19 +30,23 @@ data Quadrant
   }
   deriving (Eq, Show)
 
+quadrantCoords :: [(Int, Int)]
+quadrantCoords = [(x, y) | x <- [0..15], y <- [0..15]]
+
 initQuadrant :: Quadrant
 initQuadrant =
   let sectorStarMap
         = M.fromList
-        [ ((0, 0), [(10, 10)])
-        ]
+        [ (coord, [(10, 10)]) | coord <- quadrantCoords ]
       sectorEnemyShipMap
         = M.fromList
-        [ ((0, 0), listArray (0,0) [Enemy 8 3 20 10 Patrolling 10])
+        [ (coord, if coord == (0, 0) then listArray (0,0) [Enemy 8 3 20 10 Patrolling 10] else listArray (0, -1) [])
+        | coord <- quadrantCoords
         ]
       sectorStationMap
         = M.fromList
-        [ ((0, 0), listArray (0,0) [Station 9 1 100])
+        [ (coord, listArray (0,0) [Station 9 1 100])
+        | coord <- quadrantCoords
         ]
   in Quadrant sectorStarMap sectorEnemyShipMap sectorStationMap
 
@@ -57,31 +64,47 @@ updateSector quadrant sectorCoord Sector {..} =
            , _quadrantStations = M.adjust (const sectorStations) sectorCoord (_quadrantStations quadrant)
            }
 
-newtype QuadrantTile = QuadrantTile Int
+data QuadrantTile
+  = QuadrantTile
+  { quadrantTileHasPlayerShip :: Bool
+  , quadrantTileHasEnemyShips :: Bool
+  , quadrantTileHasStarbase   :: Bool
+  }
   deriving (Eq, Show)
+
+toQuadrantTile :: Sector -> QuadrantTile
+toQuadrantTile Sector {..}
+  = QuadrantTile
+  { quadrantTileHasPlayerShip = False -- TODO: Help! Fix me!
+  , quadrantTileHasEnemyShips = any enemyAlive . Array.elems $ sectorEnemyShips
+  , quadrantTileHasStarbase   = not . null . Array.elems $ sectorStations
+  }
+  where
+    enemyAlive Enemy {..} = enemyHitPoints > 0
 
 renderTile :: QuadrantTile -> Text
 renderTile = T.pack . show
 
-newtype QuadrantTiles = QuadrantTiles { getQuadrantTiles :: Array Int Int }
+newtype QuadrantTiles
+  = QuadrantTiles { getQuadrantTiles :: [QuadrantTile] }
+  deriving (Eq, Show)
 
 getTile :: Int -> Int -> QuadrantTiles -> Maybe QuadrantTile
 getTile x y tiles
   | x >= 0 && x <= 15 && y >= 0 && y <= 15 =
     let quadrantData = getQuadrantTiles tiles
-    in Just . QuadrantTile $ quadrantData Array.! ((y * 16) + x)
+    in Just $ quadrantData !! ((y * 16) + x)
   | otherwise = Nothing
 
 buildTiles :: Quadrant -> QuadrantTiles
-buildTiles _ = QuadrantTiles $ listArray (0, 15) [1..]
+buildTiles quadrant =
+  QuadrantTiles . map (toQuadrantTile . getSector quadrant) $ quadrantCoords
 
 render :: QuadrantTiles -> Text
 render
   = T.intercalate "\n"
   . map renderRow
   . chunksOf 4
-  . map (QuadrantTile . snd)
-  . assocs
   . getQuadrantTiles
   where
     renderRow :: [QuadrantTile] -> Text
